b51a9875ae727f3feb98c54686ba07827cc86a52
Mahesh Vanam <maheshvanam09@gmail.com>
2020-05-21 01:01:37 +0530

    Mahesh[ADDED]:added code and test case to test Sad Mood.

---
+++ MoodAnalyzer/MoodAnalyzer/Analyzer.swift
@@ -0,0 +1,14 @@
+//
+//  Analyzer.swift
+//  MoodAnalyzer
+//
+//  Created by admin on 21/05/20.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import Foundation
+class Analyzer{
+    func analyseMood(message:String)->String{
+        return "SAD"
+    }
+}
--- MoodAnalyzer/MoodAnalyzerTests/MoodAnalyzerTests.swift
+++ MoodAnalyzer/MoodAnalyzerTests/MoodAnalyzerTests.swift
@@ -10,25 +10,9 @@ import XCTest
 @testable import MoodAnalyzer
 
 class MoodAnalyzerTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func test_givenAMessage_whenGivenSadMood_ShouldReturnSAD(){
+        let analyzer=Analyzer()
+        let mood = analyzer.analyseMood(message: "i am in Sad Mood")
+        XCTAssertEqual("SAD", mood)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
